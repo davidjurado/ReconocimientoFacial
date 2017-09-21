d3909aafd5f76face65c8d29bd73cceade1775dd
David Jurado <davidblanco103@gmail.com>
2017-09-21 18:39:27 -0500

    visualización del resultado de deteccion de rostro en forma de tarjeta

--- app/config.php
+++ app/config.php
@@ -3,8 +3,8 @@
 return[
     "s3"=>[
      
-        'key' => '',
-        'secret' => '',
+        'key' => 'AKIAJMT2MAZBR3C4EPMA',
+        'secret' => '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',
         'region' => 'us-west-2',
         'bucket'=>'davidferd',
         'version' => 'latest',
--- saveawsimage.php
+++ saveawsimage.php
@@ -1,3 +1,42 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>Reconocimiento Facial</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <style type="text/css">
+        #results { float:right; margin:20px; padding:20px; border:1px solid;}
+    </style>
+</head>
+<body>
+  <nav class="navbar-fixed blue lighten-5" role="navigation">
+    <div class="nav-wrapper container ">
+
+
+
+      <a id="logo-container" href="." class="brand-logo">Captura</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Reconocimiento</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+     
+    </div>
+  </nav>
+
+
+
+
+
+
+
 <?php
 require './app/start.php';
 use Aws\S3\Exception\S3Exception;
@@ -38,9 +77,10 @@ $gestor=fopen($tmp_file_path, 'rb');
 ]);
 
       
-       $enlace=$result["@metadata"]["effectiveUri"]."\n";
-       echo "<a href='".$enlace."' target='_blank'>".$enlace."</a>";
-       echo "<br>";
+       $enlace=$result["@metadata"]["effectiveUri"];
+      // echo "<a href='".$enlace."' target='_blank'>".$enlace."</a>";
+      // echo "<br>";
+       $imageData = base64_encode(file_get_contents($enlace));
 
 $face=$rek->detectFaces([
     'Image' => [
@@ -64,21 +104,13 @@ $a=$face['FaceDetails'][0]['Confidence'];
 
 $b=json_encode($a);
 
-echo "<br>";
 
-if(!is_null($a)){
-echo "Confidencialidad FaceDetection: ";
-echo $b." %";
-}else{
-echo "No se detectó algún rostro";
-}
 
 
 echo "<br>";
 $file='FaceDetails.json';
 file_put_contents($file, $d);
-echo "<br>";
-echo "<a href='FaceDetails.json' target='_blank' >Detalles faciales</a>";
+
 
 
 $labels = $rek->detectLabels([
@@ -95,12 +127,10 @@ $labels = $rek->detectLabels([
 
 
 $l=json_encode($labels['Labels']);
-echo "<br>";
+
 $file='Labels.json';
 file_put_contents($file, $l);
-echo "<br>";
-echo "<a href='Labels.json' target='_blank' >Etiquetas</a>";
-echo "<br>";
+
 
 
 
@@ -109,9 +139,120 @@ unlink($tmp_file_path);
 
 
 
+
+
+
+if(!is_null($a)){
+
+
+echo '
+
+ <div class="row">
+<div class="col s8 offset-s1">
+
+
+
+<div class="row">
+        <div class="col s12 m7">
+          <div class="card">
+            <div class="card-image">
+              <img class="materialboxed" data-caption="'.$b." %".'" src="data:image/jpeg;base64,'.$imageData.'">
+              <span class="card-title">'.$id_img.'</span>
+            </div>
+            <div class="card-content">
+              <p>Rostro detectado exitosamente.</p>
+            </div>
+            <div class="card-tabs">
+      <ul class="tabs tabs-fixed-width">
+        <li class="tab"><a class="active" href="#test4">Detección Facial</a></li>
+        <li class="tab"><a href="#test5">Detalles Faciales</a></li>
+        <li class="tab"><a href="#test6">Etiquetas</a></li>
+      </ul>
+    </div>
+    <div class="card-content grey lighten-4">
+      <div id="test4">Confidencialidad de detección: '.$b." %".'</div>
+      <div id="test5"><a href="FaceDetails.json" target="_blank" >Detalles faciales</a></div>
+      <div id="test6"><a href="Labels.json" target="_blank" >Etiquetas</a></div>
+    </div>
+          </div>
+        </div>
+      </div>
+            
+            
+      </div>
+       </div>
+';
+
+
+
+
+
+
+
+}else{
+
+
+echo '
+
+ <div class="row">
+<div class="col s8 offset-s1">
+
+
+
+<div class="row">
+        <div class="col s12 m7">
+          <div class="card">
+            <div class="card-image">
+             <img class="materialboxed" data-caption="No se recoce ninguna cara" src="data:image/jpeg;base64,'.$imageData.'">
+              <span class="card-title">'.$id_img.'</span>
+            </div>
+            <div class="card-content">
+              <p>No se ha detectado ningún rostro.</p>
+            </div>
+            <div class="card-tabs">
+      <ul class="tabs tabs-fixed-width">
+        <li class="tab"><a class="active" href="#test4">Detección Facial</a></li>
+        <li class="tab"><a href="#test6">Etiquetas</a></li>
+      </ul>
+    </div>
+    <div class="card-content grey lighten-4">
+      <div id="test4">Confidencialidad de detección: 0% </div>
+      <div id="test6"><a href="Labels.json" target="_blank" >Etiquetas</a>
+      </div>
+    </div>
+          </div>
+        </div>
+      </div>
+            
+            
+      </div>
+       </div>
+';
+
+}
+
+
+
     } catch (S3Exception $e) {
     //die("There was an error uploading that file.");
     echo $e->getMessage() . "\n";
     }
  
+
+
+
+
 ?>
+
+
+
+
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+</body>
+</html>
